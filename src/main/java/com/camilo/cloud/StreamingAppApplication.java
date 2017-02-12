c0354a6013ac012f38df66813283c183d474aa37
Camilo Crespo <crezam@live.com>
2017-02-11 23:34:50 -0500

    Initial commit

---
+++ src/main/java/com/camilo/cloud/StreamingAppApplication.java
@@ -0,0 +1,12 @@
+package com.camilo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/camilo/cloud/StreamingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.camilo.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamingAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
