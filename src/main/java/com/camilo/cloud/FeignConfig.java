2e369d9afb74718bb1f432c9f2f58649156b7e2f
Camilo Crespo <crezam@live.com>
2017-02-12 23:53:11 -0500

    Add target interface

--- src/main/java/com/camilo/cloud/FeignConfig.java
+++ src/main/java/com/camilo/cloud/FeignConfig.java
@@ -1,7 +1,19 @@
 package com.camilo.cloud;
 
+import feign.Feign;
+import feign.codec.Decoder;
+import feign.gson.GsonDecoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    interface Github {}
+
+    @Bean
+    public Github githubDecoder() {
+        Decoder decoder = new GsonDecoder();
+        return Feign.builder().decoder(decoder).target(Github.class, "https://api.github.com");
+    }
 }
