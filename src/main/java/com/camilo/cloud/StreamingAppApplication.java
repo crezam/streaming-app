9dd4c18259c74a4d60a4e28a65b90017e1dc2189
Camilo Crespo <crezam@live.com>
2017-02-13 23:34:03 -0500

    Command line runner

--- src/main/java/com/camilo/cloud/StreamingAppApplication.java
+++ src/main/java/com/camilo/cloud/StreamingAppApplication.java
@@ -1,12 +1,18 @@
 package com.camilo.cloud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StreamingAppApplication {
+public class StreamingAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamingAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
 	}
 }
