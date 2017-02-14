260bef3e4c7e8a565704db64157e5c47dedc9914
Camilo Crespo <crezam@live.com>
2017-02-13 23:42:31 -0500

    Feign template interface

--- src/main/java/com/camilo/cloud/Github.java
+++ src/main/java/com/camilo/cloud/Github.java
@@ -1,4 +1,13 @@
 package com.camilo.cloud;
 
+import com.camilo.cloud.model.Contributor;
+import feign.Param;
+import feign.RequestLine;
+
+import java.util.List;
+
 public interface Github {
+
+    @RequestLine("GET /repos/{owner}/{repo}/contributors")
+    List<Contributor> contributors(@Param("owner") String owner, @Param("repo") String repo);
 }
