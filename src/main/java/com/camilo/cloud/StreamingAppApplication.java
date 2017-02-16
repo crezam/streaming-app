c1a1dc6ff4c6eaeb09d1eb6256e3d23248dd4b9c
Camilo Crespo <crezam@live.com>
2017-02-15 23:47:36 -0500

    Short lived task invoking Github API

--- src/main/java/com/camilo/cloud/StreamingAppApplication.java
+++ src/main/java/com/camilo/cloud/StreamingAppApplication.java
@@ -1,18 +1,32 @@
 package com.camilo.cloud;
 
+import com.camilo.cloud.model.Contributor;
+import feign.Feign;
+import feign.gson.GsonDecoder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class StreamingAppApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamingAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingAppApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
+    @Override
+    public void run(String... strings) throws Exception {
 
-	}
+        Github github = Feign.builder()
+                .decoder(new GsonDecoder())
+                .target(Github.class, "https://api.github.com");
+
+        // Fetch and print a list of the contributors to this library.
+        List<Contributor> contributors = github.contributors("docker", "docker");
+        for (Contributor contributor : contributors) {
+            System.out.println(contributor.getLogin() + " (" + contributor.getContributions() + ")");
+        }
+    }
 }
